1f5c61ce85e0437c3539f16cd092a3c43963198a
funfox1 <funfox1@yahoo.com>
2017-03-26 23:10:23 +0200

    test

--- role.bringer.js
+++ role.bringer.js
@@ -32,17 +32,9 @@ var roleBringer = {
 	            if (creep.memory.bringerfocus){
 	                 //&& Game.getObjectById(creep.memory.bringerfocus).energy<Game.getObjectById(creep.memory.bringerfocus.energyCapacity
 	                target = Game.getObjectById(creep.memory.bringerfocus);
-                  if target.energy == target.energyCapacity {
-                    var targets = creep.room.find(FIND_STRUCTURES,
-                      {filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;} } );
-                      if(targets.length > 0) {
-                        target=targets[0];
-                      }
-                    }
 	                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                             creep.moveTo(target);
                     }
-
 
 	            }
 	            else
