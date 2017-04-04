0945836fbc83cc60b9dcaa3043fa2bbcc69e187f
funfox1 <funfox1@yahoo.com>
2017-04-04 22:10:22 +0200

    gogo claim

--- role.claimer.js
+++ role.claimer.js
@@ -10,38 +10,9 @@ var roleClaimer = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        /*
-        if(creep.memory.dying && creep.ticksToLive > (CREEP_LIFE_TIME-100)) {
-            creep.memory.dying = false;
-            creep.say('back to work');
-	    }
-	    if(!creep.memory.dying && creep.ticksToLive < 150 ) {
-	        if (creep.body.length>3){
-    	        creep.memory.dying = true;
-	            creep.say('regening');
-	        }
-	    }
-
-        if(creep.memory.dying) {
-            if (creep.room.name != Game.spawns['Spawn1'].room.name){
-                creep.moveTo(creep.pos.findClosestByPath(creep.room.findExitTo(Game.spawns['Spawn1'].room.name)));
-            }
-            else{
-                creep.moveTo(Game.spawns['Spawn1']);
-            }
-
+      if(creep.reserveController(creep.room.controller)== ERR_NOT_IN_RANGE){
+                    creep.moveTo(Game.flags[creep.memory.claimFocus]);
         }
-        else{*/
-            if (creep.room.name != creep.memory.claimFocus){
-                creep.moveTo(Game.flags['funfoxClaim1']);
-                creep.say("going from "+creep.room.name+" to "+creep.memory.claimFocus);
-            }
-            else{
-                if(creep.reserveController(creep.memory.claimFocus)== ERR_NOT_IN_RANGE){
-                    creep.moveTo(creep.memory.claimFocus);
-                }
-            }
-        }
-    /*}*/
+      }
 };
 module.exports = roleClaimer;
